a0d24888ace55bd3263c99c480203926d14a3849
Steven Croft <scroft001@gmail.com>
2019-11-26 14:15:12 -0800

    Entire System built

---
+++ Teleporter/Assets/PlayerMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public Rigidbody rb;
+
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey ("d")) {
+			rb.AddForce (2f, 0f, 0f);
+		}
+		if (Input.GetKey ("a")) {
+			rb.AddForce (-2f, 0f, 0f);
+		}
+	}
+}
